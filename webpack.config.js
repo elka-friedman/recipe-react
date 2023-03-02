1705cfaa66dc8d53b4e42a2145dc35d24fbc9a70
Elka Friedman <elkafriedman1@gmail.com>
2023-03-01 19:27:42 -0500

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,21 @@
-const Dotenv = import('dotenv-webpack');
-export const plugins = [
-    new Dotenv()
-];
+const path = require("path");
+module.exports = {
+    entry: "/src/index.js",
+    output: {path: path.resolve(__dirname, "dist")},
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        preset: ["@babel/preset-env", "@babel/preset-react"],
+
+                    },
+                },
+            },
+        ],
+    },
+
+};
